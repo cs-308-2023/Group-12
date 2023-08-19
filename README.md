5edb942cb67a9c2195a32e14de047211ecc3d12a
AdiIsHappy <ad2004sahu@gmail.com>
2023-08-19 13:25:22 +0530

    this is commit for q11

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Group-12
+this is a program to calcualte the factorial of a number and prints hello before prinitng the factorial.
--- factorial.cpp
+++ factorial.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 
 int factorial(int n){
+// some random comment added to check diff
     if(n<0){
         cout <<  "please enter a positive number" <<endl;
+//this is 3rd comment
         cin >> n;
     }
     if(n!=1){
 	return(n * factorial(n-1));
     }
+//here is another comment
     else return 1;
 }
