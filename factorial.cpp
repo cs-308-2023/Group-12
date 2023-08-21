b7b7c248828eb7aac6c6f934cd9ad00a32c1afee
AdiIsHappy <ad2004sahu@gmail.com>
2023-08-21 15:31:55 +0530

    replaced cout with printf

--- factorial.cpp
+++ factorial.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int factorial(int n){
 // some random comment added to check diff
     if(n<0){
-        cout <<  "please enter a positive number" <<endl;
+        printf("please enter a positive number");
 //this is 3rd comment
         cin >> n;
     }
