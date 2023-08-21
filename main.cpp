568e50c45df51d327aed5afddd3ed83530c4045d
AdiIsHappy <ad2004sahu@gmail.com>
2023-08-21 16:29:07 +0530

    added gcd call in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ int main(){
     print_hello();
     cout<< endl;
     cout<<"The factorial of 5 is "<< factorial(-1)<< endl;
+    int x = 56, y = 98;
+    int z = gcd(x,y)
+    cout << "gcd of " << x << " & " << y << " is "<< z <<endl; 
     return 0;
 }
